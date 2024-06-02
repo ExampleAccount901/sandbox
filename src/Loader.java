03c8a030322d3e014f19c625a3506200023c9f35
Andrew <mr.andrey.kozyrev@gmail.com>
2024-06-02 16:24:40 +0300

    created a task for you

--- src/Loader.java
+++ src/Loader.java
@@ -7,8 +7,17 @@ public class Loader {
     public static void main(String[] args) {
         File root = new File("E:\\IdeaProjects\\sandbox_2");
         Collection<File> files = FileUtils.listFiles(root, null, true);
+        int count = 0;
         for (File file : files) {
-            System.out.println(file.getAbsolutePath());
+            if (file.getName().endsWith(".java"))
+                System.out.println(file.getAbsolutePath());
+                count++;
+        }
+
+        System.out.printf("Count of java files: %d%n", count);
+
+        if (count != 2) {
+            System.err.println("Try again");
         }
     }
 }
