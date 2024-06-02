72651776b47aff1b580ba643ac47f98e8ee2b376
Andrew <mr.andrey.kozyrev@gmail.com>
2024-06-02 15:13:24 +0300

    added apache commons library

--- src/Loader.java
+++ src/Loader.java
@@ -1,8 +1,14 @@
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.util.Collection;
+
 public class Loader {
     public static void main(String[] args) {
-        int i;
-        for (i = 0; i < 10; i++);{
-            System.out.println(i);
+        File root = new File("E:\\IdeaProjects\\sandbox_2");
+        Collection<File> files = FileUtils.listFiles(root, null, true);
+        for (File file : files) {
+            System.out.println(file.getAbsolutePath());
         }
     }
 }
