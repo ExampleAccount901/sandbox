c4ceb58aa6743745f94e3d6dbce3e055c004a38a
Andrew <mr.andrey.kozyrev@gmail.com>
2024-06-02 14:22:29 +0300

    update

--- src/Loader.java
+++ src/Loader.java
@@ -1,12 +1,8 @@
-import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 public class Loader {
-    public static void main(String[] args) throws IOException {
-        Path filePath = Paths.get("resources/input_small.csv");
-        FileUtils utils = new FileUtils(filePath);
-        long count = utils.countLetters();
-        System.out.println(filePath + " has " + count + " letters.");
+    public static void main(String[] args) {
+        int i;
+        for (i = 0; i < 10; i++);{
+            System.out.println(i);
+        }
     }
 }
