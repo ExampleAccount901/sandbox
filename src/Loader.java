b7c3f84748b00245468b9a8551c1b7f1370ac34e
Rasko <raskokun2015@gmail.com>
2024-06-07 22:03:01 +0300

    added changes

--- src/Loader.java
+++ src/Loader.java
@@ -7,18 +7,16 @@ public class Loader {
     public static void main(String[] args) throws IOException {
         String filePath = "resources/input_small.csv";
         BufferedReader reader = new BufferedReader(new FileReader(filePath));
-        int[] numbers = new int[1000];
+        String[] lines = new String[1000];
         int i = 0;
 
         while (reader.ready()) {
             String line = reader.readLine();
-            String[] words = line.split(",");
-            String firstWord = words[0];
-            int num = Integer.parseInt(firstWord);
-            numbers[i++] = num;
+            lines[i] = line;
+            i++;
         }
-        Arrays.sort(numbers);
-        System.out.println(Arrays.toString(numbers));
+        Arrays.sort(lines);
+        System.out.println(Arrays.toString(lines));
 
         reader.close();
     }
