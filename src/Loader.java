c351f7699575417ec8c198fffef4537dcc4fc29b
Andrew <mr.andrey.kozyrev@gmail.com>
2024-06-07 22:14:48 +0300

    added task

--- src/Loader.java
+++ src/Loader.java
@@ -1,23 +1,16 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Arrays;
-
 public class Loader {
-    public static void main(String[] args) throws IOException {
-        String filePath = "resources/input_small.csv";
-        BufferedReader reader = new BufferedReader(new FileReader(filePath));
-        String[] lines = new String[1000];
-        int i = 0;
+    public static void main(String[] args) {
+        String input = "resources/input_small.csv";
+        String output = "resources/output_sorted.csv";
+        sortFile(input,output);
+    }
 
-        while (reader.ready()) {
-            String line = reader.readLine();
-            lines[i] = line;
-            i++;
-        }
-        Arrays.sort(lines);
-        System.out.println(Arrays.toString(lines));
+    /**
+     * Sorts inputFile by index
+     * @param inputFile
+     * @param outputFile
+     */
+    public static void sortFile(String inputFile, String outputFile) {
 
-        reader.close();
     }
 }
