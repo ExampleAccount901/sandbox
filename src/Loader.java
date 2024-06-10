17987e47243044582776ca460960ae04a44fc9b1
Rasko <raskokun2015@gmail.com>
2024-06-10 18:02:36 +0300

    added changes

--- src/Loader.java
+++ src/Loader.java
@@ -1,16 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+
 public class Loader {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String input = "resources/input_small.csv";
         String output = "resources/output_sorted.csv";
-        reverseFile(input,output);
-    }
-
-    /**
-     * Берет исходный файл и записывает в новый файл все строки в обратном порядке
-     * @param inputFile
-     * @param outputFile
-     */
-    public static void reverseFile(String inputFile, String outputFile) {
+        reverseFile(input, output);
 
     }
-}
+
+
+    public static void reverseFile(String inputFile, String outputFile) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("resources/input_small.csv"));
+            for (int i = 0; i <1000; i++) {
+            String list = reader.readLine();
+                String[] myarray = list.split(",");
+                Collections.reverse(Arrays.asList(myarray));
+                String done=Arrays.toString(myarray);
+                System.out.printf(done);
+
+
+
+            }
+
+
+
+
+
+
+        }}
+
+
+
+
+
+
+
+
+
+
+
+
+
