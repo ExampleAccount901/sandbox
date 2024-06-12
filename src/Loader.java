01b65053b2f42af7a0e5fbcdabfdc1c42690e2df
Andrew <mr.andrey.kozyrev@gmail.com>
2024-06-12 13:06:31 +0300

    .

--- src/Loader.java
+++ src/Loader.java
@@ -1,18 +1,32 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.List;
 
 public class Loader {
     public static void main(String[] args) throws IOException {
         List<String> lines = Files.readAllLines(Paths.get("resources/input_small.csv"));
+        String[] temp = new String[4000];
+        String[] result = new String[4000];
+        int offset = 0;
         for (String line : lines) {
             String[] words = line.split(",");
-            String[] words2 = new String[4];
-            System.arraycopy(words, 1, words2, 0, 4);
-            String result = String.join(" ", words2);
-            System.out.println(result);
+            System.arraycopy(words, 1, temp, offset, 4);
+            offset += 4;
         }
+        int counter = 0;
+        for (int i = 0; i < temp.length; i++) {
+            if (temp[i].length() <= 5) {
+                result[counter] = temp[i];
+                counter++;
+            }
+        }
+        System.out.println(counter);
+        System.out.println(Arrays.toString(result));
+
+        String[] result2 = new String[counter];
+
     }
 }
 
